83d3b992bd53d0bdbf7d69b9fd53c160a4d03214
Maks Borenkov <borenkovmv@gmail.com>
2023-02-07 21:38:58 +0200

    new

---
+++ main.js
@@ -0,0 +1,34 @@
+// let num1 = 10;
+// let num2 = 12;
+// console.log(num1, num2);
+// [num1, num2] = [num2, num1];
+// console.log(num1, num2);
+
+// var sum = [12, 45, 2, 4, 73, 30];
+
+// sum.sort(function (a, b) {
+//   return a - b;
+// });
+// console.log(sum);
+
+// let a = [1234];
+// let b = [1234];
+
+// console.log(a === b);
+// b = a;
+// console.log(a === b);
+
+// arr = [1, 2, 3, 4, 5, 6, 7]; //Инициализируем массивы
+// tempDates = [6, 7, 8];
+
+// tempDates.forEach((elem) => arr.push(elem)); // Добавляем каждый элемент из массива 1 в массив 2
+// arr = [...new Set(arr)]; // Оставляем в массиве только уникальные значения
+
+// console.log(arr);
+
+let a = 10;
+let b = 15;
+let c = 20;
+c = b = a;
+
+console.log(c);
