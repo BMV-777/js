a762c8d722d2feccd28eab3d307021d4ff3d48d4
Maks Borenkov <borenkovmv@gmail.com>
2023-02-27 22:24:32 +0200

    операторы сравнение

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 // console.log(num1, num2);
 // [num1, num2] = [num2, num1];
 // console.log(num1, num2);
-
 // var sum = [12, 45, 2, 4, 73, 30];
 
 // sum.sort(function (a, b) {
@@ -26,9 +25,43 @@
 
 // console.log(arr);
 
-let a = 10;
-let b = 15;
-let c = 20;
-c = b = a;
+// let a = 10;
+// let b = 15;
+// let c = 20;
+// c = b = a;
 
-console.log(c);
+// console.log(c);
+
+//  let one = 10.1233;
+
+// one = Number.parseFloat(one);
+// one = Number.parseInt(one);
+
+// console.log(one)
+// console.log(one);
+// one = Number(one.toFixed(4));
+//  console.log(one)
+
+// const colors = ['tomato', 'teal', 'orange', 'green', 'blue'];
+
+// const max = colors.length - 1;
+// const min = 0;
+
+// const index = Math.round(Math.random() * (max - min) + min);
+
+// const color = colors[index];
+
+// console.log(color);
+
+// document.body.style.backgroundColor=color
+
+// const brand = "SamSung";
+
+// let one =brand[0] + brand.slice(1).toLocaleLowerCase();
+// console.log(one)
+
+if (true) {
+  var a = 5;
+}
+
+console.log(a)
