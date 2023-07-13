b9fafd1abc849c29cf90b5c8464b85633ea6f717
Maks Borenkov <borenkovmv@gmail.com>
2023-07-13 21:54:31 +0300

    dom9

--- main.js
+++ main.js
@@ -2812,46 +2812,54 @@
 
 // console.log(name);
 
-const word0 = "Good";
-const word0translate = "Отлично";
+// const word0 = "Good";
+// const word0translate = "Отлично";
 
-const word1 = "Boy";
-const word1translate = "Парень";
+// const word1 = "Boy";
+// const word1translate = "Парень";
 
-const word2 = "Pragrammist";
-const word2translate = "Праграммист";
+// const word2 = "Pragrammist";
+// const word2translate = "Праграммист";
 
-const wordLeng = 3;
+// const wordLeng = 3;
 
-const finishGoodMessage = "Молодец. Good test";
-const finishFalseMessage = "Молодец. папробуй еще все получится!";
+// const finishGoodMessage = "Молодец. Good test";
+// const finishFalseMessage = "Молодец. папробуй еще все получится!";
 
-let counter = 0;
+// let counter = 0;
 
-const procentTest = 50;
+// const procentTest = 50;
 
-const userAnswer0 = prompt(word0);
-alert(userAnswer0 === word0translate);
-if (userAnswer0 === word0translate) {
-  counter = counter += 1;
-}
+// const userAnswer0 = prompt(word0);
+// alert(userAnswer0 === word0translate);
+// if (userAnswer0 === word0translate) {
+//   counter = counter += 1;
+// }
 
-const userAnswer1 = prompt(word1);
-alert(userAnswer1 === word1translate);
-if (userAnswer1 === word1translate) {
-  counter = counter += 1;
-}
+// const userAnswer1 = prompt(word1);
+// alert(userAnswer1 === word1translate);
+// if (userAnswer1 === word1translate) {
+//   counter = counter += 1;
+// }
 
-const userAnswer2 = prompt(word2);
-alert(userAnswer2 === word2translate);
-if (userAnswer2 === word2translate) {
-  counter = counter += 1;
-}
+// const userAnswer2 = prompt(word2);
+// alert(userAnswer2 === word2translate);
+// if (userAnswer2 === word2translate) {
+//   counter = counter += 1;
+// }
 
-const countWordleng = (counter / wordLeng) * 100;
+// const countWordleng = (counter / wordLeng) * 100;
 
-if (countWordleng > procentTest) {
-  alert(finishGoodMessage);
-} else {
-  alert(finishFalseMessage);
-}
+// if (countWordleng > procentTest) {
+//   alert(finishGoodMessage);
+// } else {
+//   alert(finishFalseMessage);
+// }
+
+const user = {
+  name: "Boby",
+  age: 37,
+};
+const templete = `Hello ${user.name}! You are ${user.age} years old`;
+
+console.log(templete);
